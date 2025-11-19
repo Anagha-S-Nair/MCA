3fd94e522d1afe1e62e0b663b19d0af0bcc257e8
Anagha-S-Nair <anaghasnair596@gmail.com>
2025-11-19 15:43:51 +0530

    Add files via upload

---
+++ Web_development/dashboard.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./home";
+import About from "./about";
+import Contact from "./contact";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
