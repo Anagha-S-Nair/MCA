eb9e608c40472fa8105e71b114802a1d6d961c0e
Anagha-S-Nair <anaghasnair596@gmail.com>
2026-01-14 20:19:13 +0530

    Commit

--- Python_programs/p23.py
+++ Python_programs/p23.py
@@ -1,5 +1,5 @@
 #Write a function to get a new string from a given string by adding'ls' to the begining of the input string.
-#if the given string already begins with 'ls',rerturn the input string unchanged
+#if the given string already begins with 'ls',return the input string unchanged
 def add_is(s):
     return s if s.startswith("Is") else "Is"+s
 str=input("Enter a string: ")
