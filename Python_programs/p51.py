8b94643e8b226ab4e1dc0e6f5a8c4d758a6c4f18
Anagha-S-Nair <anaghasnair596@gmail.com>
2026-01-15 10:55:42 +0530

    Commit

--- Python_programs/p51.py
+++ Python_programs/p51.py
@@ -1,15 +1,25 @@
-#Remove a line from the file
+# Remove a line from a file
+
 inf = False
 try:
-    n=int(input("Enter line number to remove: "))
+    n = int(input("Enter line number to remove: "))
+    
     inf = open("sample.txt", "r")
     lines = inf.readlines()
     print(lines)
+    inf.close()
+
     lines.pop(n-1)
+
+    inf = open("sample.txt", "w")
     inf.writelines(lines)
     print(lines)
+
+    print("Line removed successfully")
+
 except IOError as io:
     print("File error:", io)
+
 finally:
     if inf:
         inf.close()
