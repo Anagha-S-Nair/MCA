3d47d942f4ccbdf5bd32d0cde46877db0f5a7b73
Anagha-S-Nair <anaghasnair596@gmail.com>
2026-02-15 12:07:09 +0530

    Commit

---
+++ Java_Programs/Student.java
@@ -0,0 +1,4 @@
+//Create a class 'Student' with data members Rollno,Name and Marks.Create N objects of the class and generate the ranklist
+//a. Include all type of constructors
+//b. Display the total number of students
+//c. COmpare two students by their marks.
