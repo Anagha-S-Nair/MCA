3163a77f514c1c00d930f4a638ae2375693019de
Anagha-S-Nair <anaghasnair596@gmail.com>
2026-03-16 21:12:07 +0530

    Commit

---
+++ Sem 2/Linux_Programs/Larg_three.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ $1 -gt $2 ] && [ $1 -gt $3 ]
+then 
+echo "$1 is largest"
+elif [ $2 -gt $3 ]
+then
+echo "$2 is largest"
+else
+echo "$3 is largest"
+fi
---
+++ Sem 2/Linux_Programs/command_line.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Script Name: $0"
+echo "Number of Arguments: $#"
+echo "Arguments are:"
+
+for arg in "$@"
+do
+echo $arg
+done
