d09c7a4b24ee3b35e1505e70011595cb3bc195be
Anagha-S-Nair <anaghasnair596@gmail.com>
2026-04-03 11:52:13 +0530

    Commit

--- Sem 2/Java_Programs/CPU.java
+++ Sem 2/Java_Programs/CPU.java
@@ -1,43 +1,52 @@
+//PROGRAM-04
 //Create a class CPU with attribute price. Create an inner class Processor(no of cores, manufacturer) and a static method class RAM(memory, manufacturer) inside CPU. 
 // Create an object of CPU and print details of its processor and RAM.
 
-class CPU{
+class CPU {
     int price;
-    CPU(int price){
-        this.price=price;
+
+    CPU(int price) {
+        this.price = price;
     }
-    class Processor{
+
+    class Processor {
         int cores;
         String manufacturer;
-        Processor(int cores,String manufacturer){
-            this.cores=cores;
-            this.manufacturer=manufacturer;
+
+        Processor(int cores, String manufacturer) {
+            this.cores = cores;
+            this.manufacturer = manufacturer;
         }
-        void display(){
+
+        void display() {
             System.out.println("Processor details:");
-            System.out.println("Cores: "+cores);
-            System.out.println("Manufacturer: "+manufacturer);
+            System.out.println("Cores: " + cores);
+            System.out.println("Manufacturer: " + manufacturer);
         }
     }
-    static class RAM{
+
+    static class RAM {
         int memory;
         String manufacturer;
-        RAM(int memory,String manufacturer){
-            this.memory=memory;
-            this.manufacturer=manufacturer;
+
+        RAM(int memory, String manufacturer) {
+            this.memory = memory;
+            this.manufacturer = manufacturer;
         }
-        void display(){
+
+        void display() {
             System.out.println("RAM details:");
-            System.out.println("Memory: "+memory+" GB");
-            System.out.println("Manufacturer: "+manufacturer);
+            System.out.println("Memory: " + memory + " GB");
+            System.out.println("Manufacturer: " + manufacturer);
         }
     }
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         CPU cpu = new CPU(500);
-        System.out.println("CPU Price: "+cpu.price);
-        CPU.Processor processor = cpu.new Processor(8,"Intel");
+        System.out.println("CPU Price: " + cpu.price);
+        CPU.Processor processor = cpu.new Processor(8, "Intel");
         processor.display();
-        CPU.RAM ram = new CPU.RAM(16,"Corsair");
+        CPU.RAM ram = new CPU.RAM(16, "Corsair");
         ram.display();
     }
 }
