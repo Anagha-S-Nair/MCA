00e96079d819998e55b127f210fb4187f287f831
Anagha-S-Nair <anaghasnair596@gmail.com>
2026-04-03 11:47:48 +0530

    Commit

--- Sem 2/Java_Programs/ComparableDemo.java
+++ Sem 2/Java_Programs/ComparableDemo.java
@@ -1,65 +1,72 @@
-//
-interface Comparable{
+//PROGRAM-13
+//Create an interface Comparable with methods to find volume and compare objects by their volume.
+//Create classes cuboid and cylinder and compare their objects.
+
+interface Comparable {
     double volume();
+
     int compare(Comparable other);
 }
-class Cuboid implements Comparable{
-   double length,width,height;
-   Cuboid(double length,double width,double height){
-       this.length=length;
-       this.width=width;
-       this.height=height;
-   }
-    public double volume(){
-         return length*width*height;
+
+class Cuboid implements Comparable {
+    double length, width, height;
+
+    Cuboid(double length, double width, double height) {
+        this.length = length;
+        this.width = width;
+        this.height = height;
     }
-    public int compare(Comparable other){
-        if(this.volume() > other.volume()){
+
+    public double volume() {
+        return length * width * height;
+    }
+
+    public int compare(Comparable other) {
+        if (this.volume() > other.volume()) {
             return 1;
-        }
-        else if(this.volume() < other.volume()){
+        } else if (this.volume() < other.volume()) {
             return -1;
-        }
-        else{
+        } else {
             return 0;
         }
     }
 }
-class Cylinder implements Comparable{
-    double radius,height;
-    Cylinder(double radius,double height){
-        this.radius=radius;
-        this.height=height;
+
+class Cylinder implements Comparable {
+    double radius, height;
+
+    Cylinder(double radius, double height) {
+        this.radius = radius;
+        this.height = height;
     }
-    public double volume(){
-        return 3.14*radius*radius*height;
+
+    public double volume() {
+        return 3.14 * radius * radius * height;
     }
-    public int compare(Comparable other){
-        if(this.volume() > other.volume()){
+
+    public int compare(Comparable other) {
+        if (this.volume() > other.volume()) {
             return 1;
-        }
-        else if(this.volume() < other.volume()){
+        } else if (this.volume() < other.volume()) {
             return -1;
-        }
-        else{
+        } else {
             return 0;
         }
     }
 }
-public class ComparableDemo{
-    public static void main(String args[]){
-        Cuboid c1=new Cuboid(4,5,6);
-        Cylinder c2=new Cylinder(3,7);
+
+public class ComparableDemo {
+    public static void main(String args[]) {
+        Cuboid c1 = new Cuboid(4, 5, 6);
+        Cylinder c2 = new Cylinder(3, 7);
         System.out.println("Cuboid volume: " + c1.volume());
         System.out.println("Cylinder volume: " + c2.volume());
-        int result=c1.compare(c2);
-        if (result > 0 ){
+        int result = c1.compare(c2);
+        if (result > 0) {
             System.out.println("Cuboid has larger volume");
-        }
-        else if (result < 0){
+        } else if (result < 0) {
             System.out.println("Cylinder has larger volume");
-        }
-        else{
+        } else {
             System.out.println("Both are equal");
         }
     }
